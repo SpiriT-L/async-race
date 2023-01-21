829b3de8adba1f3de9c0104698fa4b9a46b7b5a7
LeonID <lmatskevich79@gmail.com>
2023-01-21 22:35:54 +0300

    feat: add functions Winners

--- src/js/api/api.ts
+++ src/js/api/api.ts
@@ -47,3 +47,77 @@ export const startEngine = async (id: number) =>
 
 export const stopEngine = async (id: number) =>
   (await fetch(`${engine}?id=${id}&status=stopped`)).json();
+
+export const getSortOrder = (sort: string | null, order: string | null) => {
+  if (sort && order) return `&_sort=${sort}&_order=${order}`;
+};
+
+// export const getWinners = async ({
+//   page,
+//   limit = 10,
+//   sort,
+//   order,
+// }: {
+//   page: number;
+//   limit: number;
+//   sort: string | null;
+//   order: string | null;
+// }): Promise<Winners> => {
+//   const response = await fetch(
+//     `${winners}?_page=${page}&_limit=${limit}${getSortOrder(sort, order)}`
+//   );
+
+//   const items = await response.json();
+
+//   return {
+//     items: await Promise.all(
+//       items.map(async (winner: Winner) => ({
+//         ...winner,
+//         car: await getCarById(winner.id.toString()),
+//       }))
+//     ),
+//     count: response.headers.get('X-Total-Count'),
+//   };
+// };
+
+export const getWinner = async (id: number) =>
+  (await fetch(`${winners}/${id}`)).json();
+
+export const getWinnerStatus = async (id: number) =>
+  (await fetch(`${winners}/${id}`)).status;
+
+export const deletWinner = async (id: number) =>
+  (await fetch(`${winners}/${id}`, { method: 'DELETE' })).json();
+
+// export const createWinner = async (body: Winner): Promise<void> => (await featch(winnets, {
+//   method: 'POST',
+//   body: JSON.stringify(body),
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+// })).json();
+
+// export const updateWinner = async (id: number, body: Winner) : Promise<void> =>
+//   (`${winners}/${id}`,
+//   {
+//     method: 'PUT',
+//     body: JSON.stringify(body),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   }).json();
+
+// export const saveWinner = async ({
+//   id,
+//   time,
+// }: {
+//   id: number;
+//   time: number;
+// }): Promise<void> => {
+//   const winner = await getWinner(id);
+//   await updateWinner(id, {
+//     id,
+//     wins: winner.wins + 1,
+//     time: time < winner.time ? time : winner.time,
+//   });
+// };
