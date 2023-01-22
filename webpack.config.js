5cfb0bc379f3d3a777bfa6e5bb55666c14be6cdc
LeonID <lmatskevich79@gmail.com>
2023-01-22 21:29:11 +0300

    fix: fix configuration webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,183 +1,85 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const TerserWebpackPlugin = require('terser-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-// const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = !isDev;
+const srcPath = path.resolve(__dirname, 'src');
 
-const filename = (ext) =>
-  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
-const filenameimg = (ext) =>
-  isDev ? `[name]${ext}` : `[name].[contenthash]${ext}`;
+const devServer = isDev =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          port: 'auto',
+          static: {
+            directory: srcPath,
+            watch: true,
+          },
+        },
+      };
 
-// const optimization = () => {
-//   const configObj = {
-//     splitChunks: 'all',
-//   };
+const esLintPlugin = isDev => (isDev ? [] : [new ESLintPlugin({ extensions: ['ts', 'js'] })]);
 
-//   if (isProd) {
-//     configObj.minimizer = [
-//       new MiniCssExtractPlugin(),
-//       new TerserWebpackPlugin(),
-//     ];
-//   }
-
-//   return configObj;
-// };
-
-module.exports = {
-  devServer: {
-    historyApiFallback: true,
-    static: path.resolve(__dirname, 'dist'),
-    // contentBase: path.join(__dirname, 'dist'),
-    open: {
-      app: {
-        name: 'chrome',
-        arguments: ['--incognito'],
-        // arguments: ['--new-window'],
-      },
-    },
-    compress: true,
-    hot: false,
-    port: 8080,
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    main: './src/index.ts',
   },
-
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './js/main.ts',
   output: {
-    filename: `./js/${filename('js')}`,
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    assetModuleFilename: `./img/${filenameimg('[ext]')}`,
-    // assetModuleFilename: 'images/[hash][ext][query]',
-  },
-  // optimization: optimization(),
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'src/index.html'),
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: isProd,
-      },
-    }),
-    // new HTMLWebpackPlugin({
-    //   template: path.resolve(__dirname, 'src/pets.html'),
-    //   filename: 'pets.html',
-    //   inject: 'body',
-    //   minify: {
-    //     collapseWhitespace: isProd,
-    //   },
-    // }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: `./css/${filename('css')}`,
-    }),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, 'src/assets/'),
-    //       to: path.resolve(__dirname, 'dist/assets/'),
-    //     },
-    //   ],
-    // }),
-    // new ESLintPlugin(),
-  ],
-  devtool: isProd ? false : 'source-map',
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    assetModuleFilename: 'assets/[hash][ext]',
   },
   module: {
     rules: [
       {
-        test: /\.html$/,
-        loader: 'html-loader',
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
       {
         test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-            },
-          },
-          'css-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: /\.s[ac]ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                return path.relative(path.dirname(resourcePath), context) + '/';
-              },
-            },
-          },
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    autoprefixer({
-                      overrideBrowserslist: ['ie >= 8', 'last 5 version'],
-                    }),
-                    {
-                      // Options
-                    },
-                  ],
-                ],
-              },
-            },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-        // type: 'asset/resource',
-      },
-      { test: /\.ts?$/, loader: 'ts-loader' },
-
-      {
-        test: /\.(?:|png|gif|jpg|jpeg|webp|svg)$/i,
-        dependency: { not: ['url'] },
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              name: path.resolve(__dirname, `./img/${filename('[ext]')}`),
-            },
-          },
-        ],
-        type: 'javascript/auto',
-        type: 'asset/resource',
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(?:|ttf|woff|woff2|eot|otfttf|woff|woff2|eot|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: path.resolve(__dirname, `./fonts/${filename('[ext]')}`),
-            },
-          },
-        ],
-        type: 'asset/resource',
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
-};
+  experiments: {
+    topLevelAwait: true,
+  },
+  plugins: [
+    ...esLintPlugin(development),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new HtmlWebpackPlugin({
+      title: 'MatchMatch',
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }],
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+  ...devServer(development),
+});
